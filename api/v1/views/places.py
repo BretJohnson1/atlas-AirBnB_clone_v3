d8fd50546dfdac9b0c198a2f3d342780ff6693ad
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-07-12 12:09:23 -0500

    too many blank lines

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -46,8 +46,6 @@ def place_all(city_id):
     # return jsonify([place.to_dict() for place in city.places])
 
 
-
-
 @app_views.route("/places/<place_id>", methods=['GET'],
                  strict_slashes=False)
 def place_by_id(place_id=None):
