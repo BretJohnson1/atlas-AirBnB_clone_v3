01ed1aa0cba4d2cc6658361cf152a36a4ea2eb87
jaylenperez <jaylenestella24@gmail.com>
2024-07-12 12:22:20 -0500

    added get and count methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,9 +80,19 @@ class DBStorage:
         Returns the object based on the class and its ID,
         or None if not found.
         """
+        if cls and id:
+            return self.__session.query(cls).get(id)
+        return None
     
     def count(self, cls=None):
         """
         Returns the number of objects in storage matching the given class.
         If no class is passed, returns the count of all objects in storage.
         """
+        if cls:
+            return self.__session.query(cls).count()
+        else:
+            total_count = 0
+            for _cls in self.all().keys():
+                total_count += self.__session.query(_cls).count()
+            return total_count
